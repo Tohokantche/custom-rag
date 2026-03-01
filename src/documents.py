6ab65e24bea52b9dfa09cf79e67f157f0f7b02dc
Aurele Gnanha <aurele.gnanha@gmail.com>
2026-03-01 21:15:27 +0800

    clean up method process_and_store_pdf and add emb_store class variables

--- src/documents.py
+++ src/documents.py
@@ -1,15 +1,11 @@
 
 import logging
-import tempfile, os
+import os
 from datetime import datetime
 import pdfplumber
-import shutil
 from typing import List, Any
 import streamlit as st
-from langchain_community.document_loaders import UnstructuredPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_ollama import OllamaEmbeddings
-from langchain_community.vectorstores import Chroma
+from src.embeddings import EmbeddingsStore
 
 PERSIST_DIRECTORY = os.path.join("data", "vectors")
 logger = logging.getLogger(__name__)
@@ -18,56 +14,28 @@ logger = logging.getLogger(__name__)
 class DocumentProcessor:
     
     # Session state
-    session_state= None
+    session_state = None
+    emb_store = None
 
     def __init__(self, session_state):
+
         DocumentProcessor.session_state = session_state
-    
+        DocumentProcessor.emb_store = EmbeddingsStore(session_state)
+
     @staticmethod
     def process_and_store_pdf(file_upload, pdf_id: str, is_sample: bool = False):
+        
         """Process single PDF and store in session state."""
+       
         logger.info(f"Processing PDF: {file_upload.name} with ID: {pdf_id}")
-
         # Create temp directory
         temp_dir = os.path.join("data","pdfs")
         if not os.path.exists(temp_dir):
             os.makedirs(os.path.join("data","pdfs"))
         path = os.path.join(temp_dir, file_upload.name)
 
-        # Save file
-        with open(path, "wb") as f:
-            f.write(file_upload.getvalue())
-            logger.info(f"File saved to temporary path: {path}")
-
-        # Load and chunk
-        loader = UnstructuredPDFLoader(path)
-        data = loader.load()
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=int(os.getenv("chunk_size",7000)),
-                                                        chunk_overlap=int(os.getenv("chunk_overlap", 100)))
-        chunks = text_splitter.split_documents(data)
-        logger.info(f"Document split into {len(chunks)} chunks")
-
-        # Add metadata to EACH chunk
-        for i, chunk in enumerate(chunks):
-            chunk.metadata.update({
-                "pdf_id": pdf_id,
-                "pdf_name": file_upload.name,
-                "chunk_index": i,
-                "source_file": file_upload.name
-            })
-
-        # Create vector DB with unique collection
-        collection_name = f"pdf_{abs(hash(file_upload.name + pdf_id))}"
-        logger.info(f"Creating vector DB with collection name: {collection_name}")
-
-        embeddings = OllamaEmbeddings(model="nomic-embed-text")
-        vector_db = Chroma.from_documents(
-            documents=chunks,
-            embedding=embeddings,
-            persist_directory=PERSIST_DIRECTORY,
-            collection_name=collection_name
-        )
-        logger.info("Vector DB created with persistent storage")
+        ### Create data store
+        vector_db = DocumentProcessor.emb_store.create_vector_db(file_upload, pdf_id)
 
         # Extract PDF pages
         with pdfplumber.open(file_upload if not is_sample else path) as pdf:
@@ -80,14 +48,13 @@ class DocumentProcessor:
             "vector_db": vector_db,
             "pages": pdf_pages,
             "file_upload": file_upload,
-            "collection_name": collection_name,
+            "collection_name": vector_db._collection.name,
             "upload_timestamp": datetime.now(),
-            "doc_count": len(chunks),
+            "doc_count": len(vector_db.get()['documents']),
             "is_sample": is_sample
         }
-        #st.session_state["active_pdfs"].append(pdf_id)
         DocumentProcessor.session_state["active_pdfs"].insert(0, pdf_id)
-        logger.info(f"PDF stored in session state with {len(chunks)} chunks")
+        logger.info(f"PDF stored in session state with {len(vector_db.get()['documents'])} chunks")
 
         # Cleanup
         # shutil.rmtree(temp_dir)
@@ -116,6 +83,7 @@ class DocumentProcessor:
     @staticmethod
     def delete_pdf(pdf_id: str):
         """Delete single PDF and its collection."""
+
         if pdf_id in DocumentProcessor.session_state["pdfs"]:
             pdf_data = st.session_state["pdfs"][pdf_id]
             logger.info(f"Deleting PDF: {pdf_data['name']} (ID: {pdf_id})")
@@ -136,6 +104,7 @@ class DocumentProcessor:
     @staticmethod
     def delete_all_pdfs():
         """Delete all PDFs."""
+        
         logger.info("Deleting all PDFs")
         for pdf_id in list(DocumentProcessor.session_state["pdfs"].keys()):
             DocumentProcessor.delete_pdf(pdf_id)
