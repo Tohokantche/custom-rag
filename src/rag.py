3192892db2c4793db95852a715e909d2794d543f
Aurele Gnanha <aurele.gnanha@gmail.com>
2026-04-04 14:12:29 +0800

    refactor: adapt code to prompt manager class

--- src/rag.py
+++ src/rag.py
@@ -1,6 +1,7 @@
 import logging, copy
 import streamlit as st
 from typing import Dict, List
+from src.prompt import PromptManager
 from langchain_ollama import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate, PromptTemplate, HumanMessagePromptTemplate
 from langchain_core.runnables import RunnablePassthrough, RunnableLambda
@@ -20,41 +21,31 @@ class RagLogic:
         THE ACTUAL RAG LOGIC
         
     """
+    prompt_manager : PromptManager = PromptManager()
 
     def __init__(self, selected_model: str, router_model:str="lfm2.5-thinking:1.2b"):
     
-        # Template for the queries router/guardrails
-        self.router_template = """
-            Accurately classify the query into exactly one category: 
-            'retrieval', 'casual', toxic, or 'unsure'. You must use previous interactions as a guidance.
-
-            - 'retrieval': explicit request for external concepts/facts/data that requires specific document/database access
-            - 'casual':  small talk/greetings/casual chat/personal conversation
-            - 'toxic':  hate speech/explicit insult/disrespectful statement/malicious intention
-            - 'unsure':  small talk that are ambiguous or unclear or vague
-                                                                                                               
-            Query: {question}
-            Category:"""
-
         self.chat_model = ChatOllama(model=selected_model, temperature=0.6, top_p=0.95, top_k=20)
         try :
-            self.router_model = ChatOllama(model=router_model, temperature=0.1, top_k=50)
+            self.router_model = ChatOllama(model=router_model, temperature=0.1, top_k=20)
         except Exception as e:
             logger.error(f"Unable to load the router model {router_model}: {e}")
             logger.warning(f"Falling back to the chat model {selected_model}.")
             self.router_model = ChatOllama(model=selected_model, temperature=0.1)
             st.warning('Using the chat model as a router. Performance might degrade and responses might be slow !', icon="⚠️")
 
-        self.chat_context = None
+        self.prune_chat_context = None
         self.sources = None
         self.response= None
 
-
     def format_chat_history(self, chat_history):
-        """ Turn List of chat history into a String"""
+        """ 
+        Turn List of chat history into a String and prune special characters
+        return a chat history as a string
+        """
         formated_chat_history = "\n".join([f"{chat.type.capitalize()}: {chat.content}" 
                           for chat in chat_history if chat.type.capitalize() != "system"])
-        # Removing curly brace to prevent Langchain prompttemplate processing error 
+        # Removing curly brace to prevent potential Langchain prompttemplate processing error 
         return formated_chat_history.replace("{", "").replace("}", "")
 
     def retrieve_doc(
@@ -63,23 +54,16 @@ class RagLogic:
         pdfs_dict: Dict[str, Dict],
         chat_context: List,
         config : Dict = None
-    ):
-        
-        """Query across multiple PDFs with source attribution."""
-        logger.info(f"Processing question across {len(pdfs_dict)} PDFs: {question}")
-        
-        # Query prompt for querry expansion. Chat history is appended for contextualization
-        template="""You are a helpful AI language model assistant. Your task is to generate exactly 3
-            different versions of the given user question to retrieve only relevant documents from a vector 
-            database. By generating multiple semantically faithful perspectives on the user question, your 
-            goal is to help the user overcome some of the limitations of the distance-based similarity search. 
-            Provide these alternative questions separated by newlines, and stick as much as you can to the 
-            semantic information of the original question without hallucinating. You must use the chat 
-            history to contextualize the question.
+    ): 
+        """
+        Retrieve relevant chunks across multiple PDFs documents with source attribution.
+        return retrieved PDFs chunk and a formatted bundel of chunk and sources
+        """
 
-            'Original question':
-            {question}
-            """ + f"\n'Chat History': \n{self.format_chat_history(chat_context)}"
+        logger.info(f"Processing question across {len(pdfs_dict)} PDFs: {question}")
+        # Query prompt for querry expansion. Chat history is appended for query contextualization
+        template = RagLogic.prompt_manager.MULTI_QUERY_PROMPT_TEMPLATE + \
+              f"\n'Chat History': \n{self.format_chat_history(chat_context)}"
         
         QUERY_PROMPT = PromptTemplate.from_template(template)
         
@@ -124,6 +108,7 @@ class RagLogic:
                     if "pdf_id" not in doc.metadata:
                         doc.metadata["pdf_id"] = pdf_id
                     if config['re_ranker'] and config :
+                        # Temporaly separately  store querry_doc_pairs and sources to match the re-ranker input format
                         temp_querry_docs_pairs.append((question, doc.page_content))
                         temp_sources.append(doc.metadata)
                 all_retrieved_docs.extend(docs)
@@ -133,7 +118,7 @@ class RagLogic:
 
         logger.info(f"Total documents retrieved: {len(all_retrieved_docs)}")
 
-        # Initialize context formating var with source labels
+        # Initialize context (doc + source) formating variable
         context_parts = []
 
         # Re-rankers the retrieved documents
@@ -142,7 +127,6 @@ class RagLogic:
             logger.info(f"Re-ranking the retreived documents")
             re_rank_documents = config['re_ranker'].get_ranked_document(temp_querry_docs_pairs, temp_sources)
             for score, doc, source in re_rank_documents:
-
                 # Keep only documents/chucks with similarity greater or equal to 0.35
                 if score >= 0.35:
                     context_parts.append(f"[Source: {source.get('pdf_name', 'Unknown')}]\n{doc}\n")
@@ -153,6 +137,7 @@ class RagLogic:
                 context_parts.append(f"[Source: {source}\n{doc.page_content}\n")
         return  "\n---\n".join(context_parts), all_retrieved_docs
     
+    
     def process_question_multi_pdf(
         self,
         question: str,
@@ -160,25 +145,29 @@ class RagLogic:
         chat_context: List,
         config : Dict = None
     ):
-        self.chat_context = copy.deepcopy(
+        """
+        Process the user query by acessing all available documents
+        return assistant/AI response asynchonously
+        """
+
+        self.prune_chat_context = copy.deepcopy(
             chat_context if len(chat_context) < 8 else chat_context[-7:]
             )
         formatted_context, all_retrieved_docs = None, None
 
         logger.info(f"Classifying the user query: {question}")
         router_messages = [
-                    SystemMessage(content=" You are a user intent detector. Your role is to accurately " \
-                    "classify the user intent without hallucinating.")
+                    SystemMessage(content= RagLogic.prompt_manager.ROUTER_SYSTEM_PROMPT)
                 ]
         
-        # By default classify queries as casual
+        # By default classify users' queries as casual
         router_response = "casual"
-        prune_context = chat_context if len(chat_context) < 5 else chat_context[-4:]
+        prune_router_context = chat_context if len(chat_context) < 5 else chat_context[-4:]
         # Only route queries with more than 12 characters to prevent unecessary computation
         if not (len(question) < 12):
 
-            router_messages.extend(prune_context)
-            router_messages.append(HumanMessagePromptTemplate.from_template(self.router_template))
+            router_messages.extend(prune_router_context)
+            router_messages.append(HumanMessagePromptTemplate.from_template(RagLogic.prompt_manager.ROUTER_PROMPT_TEMPLATE))
             router_chain = (
                 {"question": lambda x: x["question"]}
                 | ChatPromptTemplate.from_messages(router_messages)
@@ -187,51 +176,26 @@ class RagLogic:
             )
             router_response = router_chain.invoke({"question": question})
 
+        # The logic of the router of queries
         if "retrieval" in router_response:
             logger.info(f"'{question}' is clasified as a retrival query.")
-            formatted_context, all_retrieved_docs = self.retrieve_doc(question, pdfs_dict, prune_context, config)
-            
+            formatted_context, all_retrieved_docs = self.retrieve_doc(question, pdfs_dict, prune_router_context, config)
             # RAG prompt with source awareness
-            template = """Answer the question based ONLY on the following context from one or multiple documents.
-                Please only provide HIGHLY relevant information and do not hallucinate.
-                Each section is marked with its source document.
-                Each document start with its source name: 'Source:'.  
-
-                When answering:
-                1. Cite ONLY the relevant source(s) document name for each piece of information 
-                2. If information comes from multiple sources and you used them in your response, mention all relevant sources
-                3. If sources contradict, note the discrepancy and cite both
-                
-                Context:
-                {context}
-
-                Question:
-                {question}
-
-                Answer (include source citations):"""
-            
+            router_category_template = RagLogic.prompt_manager.ROUTER_RETRIEVAL_PROMPT_TEMPLATE
             logger.info("Generating response with source attribution")
-        
         elif "casual" in router_response:
             logger.info(f"'{question}' is clasified as a casual query.")
-            template = """
-                Respond casually, or respond based on previous interactions to: {question}
-            """
+            router_category_template = RagLogic.prompt_manager.ROUTER_CASUAL_PROMPT_TEMPLATE
         elif "toxic" in router_response:
             logger.info(f"'{question}' is clasified as a toxic/malicious query.")
-            template = """
-                You are a helpful assistant. Respond vaguely to : {question}
-            """
+            router_category_template = RagLogic.prompt_manager.ROUTER_TOXIC_PROMPT_TEMPLATE
         else:
             logger.info(f"'{question}' is clasified as unsure query intent.")
-            template = """
-                 You can access some external documents. Respond politely, or request politely for clarification about the query 
-                 using or not using previous interactions : {question}
-            """
+            router_category_template = RagLogic.prompt_manager.ROUTER_UNSURE_PROMPT_TEMPLATE
             
-        self.chat_context.append(HumanMessagePromptTemplate.from_template(template))
+        self.prune_chat_context.append(HumanMessagePromptTemplate.from_template(router_category_template))
         chat_context.append(HumanMessage(content=question))
-        decision_chain = ChatPromptTemplate.from_messages(self.chat_context) | self.chat_model
+        decision_chain = ChatPromptTemplate.from_messages(self.prune_chat_context) | self.chat_model
 
         response_chain = (
             {"context": lambda x: formatted_context, "question": lambda x: x["question"]} 
@@ -239,6 +203,7 @@ class RagLogic:
             | StrOutputParser()
             )
 
+        # Returning the assistance response
         response = ""
         for chunk in response_chain.stream({"question": question}):
                 response+= chunk
